30a40906384816ab0c3bf113ac033d637081f7b4
Dania <daniadeyar@gmail.com>
2022-01-27 22:48:14 +0300

    basic-strcuture

--- src/App.js
+++ src/App.js
@@ -4,8 +4,17 @@ import { FaQuoteRight } from "react-icons/fa";
 import data from "./data";
 function App() {
   const [people, setPeople] = useState(data);
-  const [index, setindex] = useState(0);
+  const [index, setIndex] = useState(0);
 
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > 0) {
+      setIndex(0);
+    }
+  }, [index, people]);
   return (
     <section className="section">
       <div className="title">
@@ -16,9 +25,18 @@ function App() {
       <div className="section-center">
         {people.map((person, personIndex) => {
           const { id, image, name, title, quote } = person;
-
+          let position = "nextSlide";
+          if (personIndex === index) {
+            position = "activeSlide";
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
           return (
-            <article key={id}>
+            <article className={position} key={id}>
               <img src={image} alt={name} className="person-img" />
               <h4>{name}</h4>
               <p className="title">{title}</p>
@@ -27,10 +45,10 @@ function App() {
             </article>
           );
         })}
-        <button className="prev">
+        <button className="prev" onClick={() => setIndex(index - 1)}>
           <FiChevronLeft />
         </button>
-        <button className="next">
+        <button className="next" onClick={() => setIndex(index + 1)}>
           <FiChevronRight />
         </button>
       </div>
